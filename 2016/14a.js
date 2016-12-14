f3df21d4f6ed9996934d6892ebef8c8b407010a8
Joe Whitfield-Seed <joeseed86@gmail.com>
2016-12-14 19:19:59 +0000

    Day 14a in JavaScript

---
+++ 2016/14a.js
@@ -0,0 +1,43 @@
+const md5 = require('md5')
+
+const INPUT = 'abc'
+
+function hash (i) {
+  return md5(`${INPUT}${i}`)
+}
+
+function isKey (i) {
+  const hash = table[i]
+  const has3 = hash.match(/(.)\1\1/)
+  if (has3) {
+    for (let j = i + 1; j < i + 1001; j++) {
+      if (table[j].match(`${has3[1]}{5}`)) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+// generate a lookup table upfront
+const N = 1e5
+const table = new Array(N)
+
+for (let i = 0; i < N + 1000; i++) {
+  table[i] = hash(i)
+}
+
+let found = 0
+for (let i = 0; i < N; i++) {
+  if (isKey(i)) {
+    console.log(i)
+    found++
+  }
+
+  if (found === 64) {
+    break
+  }
+}
+
+console.log(`Found ${found} keys in ${N} hashes`)
