3ee86b3ad3b7bbb57325ad8bfdf87e498fe212fc
Joe Whitfield-Seed <joeseed86@gmail.com>
2023-12-03 21:03:14 +0000

    Re-run 2016 day 21-22a

--- 2016/21b.js
+++ 2016/21b.js
@@ -1,151 +1,153 @@
 // Day 21b hack
 
-function swap (i, j, chars) {
-  const lastI = chars[i]
-  chars[i] = chars[j]
-  chars[j] = lastI
-  return chars
+function swap(i, j, chars) {
+  const lastI = chars[i];
+  chars[i] = chars[j];
+  chars[j] = lastI;
+  return chars;
 }
 
-function swapChars (a, b, chars) {
-  return chars.map(c => {
-    if (c === a) return b
-    if (c === b) return a
-    return c
-  })
+function swapChars(a, b, chars) {
+  return chars.map((c) => {
+    if (c === a) return b;
+    if (c === b) return a;
+    return c;
+  });
 }
 
-function rotate (dir, n, chars) {
-  const start = n % chars.length
-  if (dir === 'left') {
-    return chars.concat(chars).slice(start, start + chars.length)
-  } else if (dir === 'right') {
-    return rotate('left', chars.length - (n % chars.length), chars)
+function rotate(dir, n, chars) {
+  const start = n % chars.length;
+  if (dir === "left") {
+    return chars.concat(chars).slice(start, start + chars.length);
+  } else if (dir === "right") {
+    return rotate("left", chars.length - (n % chars.length), chars);
   } else {
-    throw new Error(`wat ${dir}`)
+    throw new Error(`wat ${dir}`);
   }
 }
 
-function rotatePosition (c, chars) {
-  const i = chars.indexOf(c)
-  const boost = i >= 4 ? 2 : 1
-  return rotate('right', i + boost, chars)
+function rotatePosition(c, chars) {
+  const i = chars.indexOf(c);
+  const boost = i >= 4 ? 2 : 1;
+  return rotate("right", i + boost, chars);
 }
 
-function reverseRange (start, end, chars) {
-  const range = chars.slice(start, end + 1).reverse()
-  chars.splice(start, range.length, ...range)
-  return chars
+function reverseRange(start, end, chars) {
+  const range = chars.slice(start, end + 1).reverse();
+  chars.splice(start, range.length, ...range);
+  return chars;
 }
 
-function move (x, y, chars) {
-  const [moved] = chars.splice(x, 1)
-  chars.splice(y, 0, moved)
-  return chars
+function move(x, y, chars) {
+  const [moved] = chars.splice(x, 1);
+  chars.splice(y, 0, moved);
+  return chars;
 }
 
-function execute (input) {
-  input = move(0, 3, input)
-  input = rotate('right', 0, input)
-  input = rotate('right', 1, input)
-  input = move(1, 5, input)
-  input = swapChars('h', 'b', input)
-  input = reverseRange(1, 3, input)
-  input = swapChars('a', 'g', input)
-  input = swapChars('b', 'h', input)
-  input = rotatePosition('c', input)
-  input = swapChars('d', 'c', input)
-  input = rotatePosition('c', input)
-  input = swap(6, 5, input)
-  input = rotate('right', 7, input)
-  input = swapChars('b', 'h', input)
-  input = move(4, 3, input)
-  input = swap(1, 0, input)
-  input = swap(7, 5, input)
-  input = move(7, 1, input)
-  input = swapChars('c', 'a', input)
-  input = move(7, 5, input)
-  input = rotate('right', 4, input)
-  input = swap(0, 5, input)
-  input = move(3, 1, input)
-  input = swapChars('c', 'h', input)
-  input = rotatePosition('d', input)
-  input = reverseRange(0, 2, input)
-  input = rotatePosition('g', input)
-  input = move(6, 7, input)
-  input = move(2, 5, input)
-  input = swap(1, 0, input)
-  input = swapChars('f', 'c', input)
-  input = rotate('right', 1, input)
-  input = reverseRange(2, 4, input)
-  input = rotate('left', 1, input)
-  input = rotatePosition('h', input)
-  input = rotate('right', 1, input)
-  input = rotate('right', 5, input)
-  input = swap(6, 3, input)
-  input = move(0, 5, input)
-  input = swapChars('g', 'f', input)
-  input = reverseRange(2, 7, input)
-  input = reverseRange(4, 6, input)
-  input = swap(4, 1, input)
-  input = move(2, 1, input)
-  input = move(3, 1, input)
-  input = swapChars('b', 'a', input)
-  input = rotatePosition('b', input)
-  input = reverseRange(3, 5, input)
-  input = move(0, 2, input)
-  input = rotatePosition('b', input)
-  input = reverseRange(4, 5, input)
-  input = rotatePosition('g', input)
-  input = reverseRange(0, 5, input)
-  input = swapChars('h', 'c', input)
-  input = reverseRange(2, 5, input)
-  input = swap(7, 5, input)
-  input = swapChars('g', 'd', input)
-  input = swapChars('d', 'e', input)
-  input = move(1, 2, input)
-  input = move(3, 2, input)
-  input = swapChars('d', 'g', input)
-  input = swap(3, 7, input)
-  input = swapChars('b', 'f', input)
-  input = rotate('right', 3, input)
-  input = move(5, 3, input)
-  input = move(1, 2, input)
-  input = rotatePosition('b', input)
-  input = rotatePosition('c', input)
-  input = reverseRange(2, 3, input)
-  input = move(2, 3, input)
-  input = rotate('right', 1, input)
-  input = move(7, 0, input)
-  input = rotate('right', 3, input)
-  input = move(6, 3, input)
-  input = rotatePosition('e', input)
-  input = swapChars('c', 'b', input)
-  input = swapChars('f', 'd', input)
-  input = swap(2, 5, input)
-  input = swapChars('f', 'g', input)
-  input = rotatePosition('a', input)
-  input = reverseRange(3, 4, input)
-  input = rotate('left', 7, input)
-  input = rotate('left', 6, input)
-  input = swapChars('g', 'b', input)
-  input = reverseRange(3, 6, input)
-  input = rotate('right', 6, input)
-  input = rotatePosition('c', input)
-  input = rotatePosition('b', input)
-  input = rotate('left', 1, input)
-  input = reverseRange(3, 7, input)
-  input = swapChars('f', 'g', input)
-  input = swap(4, 1, input)
-  input = rotatePosition('d', input)
-  input = move(0, 4, input)
-  input = swap(7, 6, input)
-  input = rotate('right', 6, input)
-  input = rotatePosition('e', input)
-  input = move(7, 3, input)
-  input = rotate('right', 3, input)
-  input = swap(1, 2, input)
-  return input
+function execute(input) {
+  // generated by 21b_helper.js
+  input = rotate("right", 3, input);
+  input = swapChars("b", "a", input);
+  input = move(3, 4, input);
+  input = swap(0, 7, input);
+  input = swapChars("f", "h", input);
+  input = rotatePosition("f", input);
+  input = rotatePosition("b", input);
+  input = swap(3, 0, input);
+  input = swap(6, 1, input);
+  input = move(4, 0, input);
+  input = rotatePosition("d", input);
+  input = swapChars("d", "h", input);
+  input = reverseRange(5, 6, input);
+  input = rotatePosition("h", input);
+  input = reverseRange(4, 5, input);
+  input = move(3, 6, input);
+  input = rotatePosition("e", input);
+  input = rotatePosition("c", input);
+  input = rotate("right", 2, input);
+  input = reverseRange(5, 6, input);
+  input = rotate("right", 3, input);
+  input = rotatePosition("b", input);
+  input = rotate("right", 5, input);
+  input = swap(5, 6, input);
+  input = move(6, 4, input);
+  input = rotate("left", 0, input);
+  input = swap(3, 5, input);
+  input = move(4, 7, input);
+  input = reverseRange(0, 7, input);
+  input = rotate("left", 4, input);
+  input = rotatePosition("d", input);
+  input = rotate("left", 3, input);
+  input = swap(0, 7, input);
+  input = rotatePosition("e", input);
+  input = swapChars("e", "a", input);
+  input = rotatePosition("c", input);
+  input = swap(3, 2, input);
+  input = rotatePosition("d", input);
+  input = reverseRange(2, 4, input);
+  input = rotatePosition("g", input);
+  input = move(3, 0, input);
+  input = move(3, 5, input);
+  input = swapChars("b", "d", input);
+  input = reverseRange(1, 5, input);
+  input = reverseRange(0, 1, input);
+  input = rotatePosition("a", input);
+  input = reverseRange(2, 5, input);
+  input = swap(1, 6, input);
+  input = swapChars("f", "e", input);
+  input = swap(5, 1, input);
+  input = rotatePosition("a", input);
+  input = move(1, 6, input);
+  input = swapChars("e", "d", input);
+  input = reverseRange(4, 7, input);
+  input = swap(7, 5, input);
+  input = swapChars("c", "g", input);
+  input = swapChars("e", "g", input);
+  input = rotate("left", 4, input);
+  input = swapChars("c", "a", input);
+  input = rotate("left", 0, input);
+  input = swap(0, 1, input);
+  input = reverseRange(1, 4, input);
+  input = rotatePosition("d", input);
+  input = swap(4, 2, input);
+  input = rotate("right", 0, input);
+  input = swap(1, 0, input);
+  input = swapChars("c", "a", input);
+  input = swap(7, 3, input);
+  input = swapChars("a", "f", input);
+  input = reverseRange(3, 7, input);
+  input = rotate("right", 1, input);
+  input = swapChars("h", "c", input);
+  input = move(1, 3, input);
+  input = swap(4, 2, input);
+  input = rotatePosition("b", input);
+  input = reverseRange(5, 6, input);
+  input = move(5, 3, input);
+  input = swapChars("b", "g", input);
+  input = rotate("right", 6, input);
+  input = reverseRange(6, 7, input);
+  input = swap(2, 5, input);
+  input = rotatePosition("e", input);
+  input = swap(1, 7, input);
+  input = swap(1, 5, input);
+  input = reverseRange(2, 7, input);
+  input = reverseRange(5, 7, input);
+  input = rotate("left", 3, input);
+  input = rotatePosition("b", input);
+  input = rotate("left", 3, input);
+  input = swapChars("e", "c", input);
+  input = rotatePosition("a", input);
+  input = swapChars("f", "a", input);
+  input = swap(0, 6, input);
+  input = swap(4, 7, input);
+  input = reverseRange(0, 5, input);
+  input = reverseRange(3, 5, input);
+  input = swapChars("d", "e", input);
+  input = move(0, 7, input);
+  input = move(1, 3, input);
+  input = reverseRange(4, 7, input);
+
+  return input;
 }
 
 // sanity check
@@ -170,20 +172,20 @@ function permute(input) {
     input.splice(i, 0, ch);
     usedChars.pop();
   }
-  return permArr
-};
+  return permArr;
+}
 
-const target = 'fbgdceah'.split('');
+const target = "fbgdceah".split("");
 const permutations = permute(target);
 
-for (let i = 0; i< permutations.length; i ++) {
+for (let i = 0; i < permutations.length; i++) {
   // most of the hash mutate their argument
-  let test = permutations[i].slice()
-  let p = execute(test)
- 
+  let test = permutations[i].slice();
+  let p = execute(test);
+
   // lazy array equality, quick enough
   if (p.toString() === target.toString()) {
-    console.log(permutations[i].join(""))
-    break
+    console.log(permutations[i].join(""));
+    break;
   }
 }
--- 2016/21b_helper.js
+++ 2016/21b_helper.js
@@ -1,4 +1,4 @@
-const { loadInput } = require('./lib.js')
+const { loadInput } = require("./lib.js");
 
 // helper script to generate inline function calls for 21b
 // slightly faster than a for loop... which turned out not to be necessary
@@ -6,38 +6,48 @@ const { loadInput } = require('./lib.js')
 // that reduces the search space from 8**8 to 8!, so per-row efficienty
 // becomes irrelevant.
 
-function toCallString (line) {
-  let match
-  if (match = line.match(/swap position (\d+) with position (\d+)/)) {
-    return `swap(${parseInt(match[1], 10)}, ${parseInt(match[2], 10)}, input)`
+function toCallString(line) {
+  let match;
+  if ((match = line.match(/swap position (\d+) with position (\d+)/))) {
+    return `input = swap(${parseInt(match[1], 10)}, ${parseInt(
+      match[2],
+      10
+    )}, input)`;
   }
 
-  if (match = line.match(/swap letter (\w+) with letter (\w+)/)) {
-    return `swapChars('${match[1]}', '${match[2]}', input)`
+  if ((match = line.match(/swap letter (\w+) with letter (\w+)/))) {
+    return `input = swapChars('${match[1]}', '${match[2]}', input)`;
   }
 
-  if (match = line.match(/rotate (left|right) (\d+) steps?/)) {
-    return `rotate('${match[1]}', ${parseInt(match[2], 10)}, input)`
+  if ((match = line.match(/rotate (left|right) (\d+) steps?/))) {
+    return `input = rotate('${match[1]}', ${parseInt(match[2], 10)}, input)`;
   }
 
-  if (match = line.match(/rotate based on position of letter (\w+)/)) {
-    return `rotatePosition('${match[1]}', input)`
+  if ((match = line.match(/rotate based on position of letter (\w+)/))) {
+    return `input = rotatePosition('${match[1]}', input)`;
   }
 
-  if (match = line.match(/reverse positions (\d) through (\d)/)) {
-    return `reverseRange(${parseInt(match[1], 10)}, ${parseInt(match[2], 10)}, input)`
+  if ((match = line.match(/reverse positions (\d) through (\d)/))) {
+    return `input = reverseRange(${parseInt(match[1], 10)}, ${parseInt(
+      match[2],
+      10
+    )}, input)`;
   }
 
-  if (match = line.match(/move position (\d) to position (\d)/)) {
-    return `move(${parseInt(match[1], 10)}, ${parseInt(match[2], 10)}, input)`
+  if ((match = line.match(/move position (\d) to position (\d)/))) {
+    return `input = move(${parseInt(match[1], 10)}, ${parseInt(
+      match[2],
+      10
+    )}, input)`;
   }
 
-  throw Error(`unknown line ${line}`)
+  throw Error(`unknown line ${line}`);
 }
 
-const calls = loadInput('21.txt')
+const calls = loadInput("21.txt")
   .trim()
   .split("\n")
-  .map(toCallString).join("\n")
-  
-console.log(calls)
+  .map(toCallString)
+  .join("\n");
+
+console.log(calls);
